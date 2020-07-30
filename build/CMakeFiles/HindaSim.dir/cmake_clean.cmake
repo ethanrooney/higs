3834d88cf77e45ab848611dfe7780af0f73bf3f0
Ethan Rooney <ethan.f.rooney@protonmail.com>
2020-07-30 14:22:52 -0400

    "cleanup build dir"

--- build/CMakeFiles/HindaSim.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/HindaSim"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/HindaSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- y89_include/MoreUnits.hh
+++
@@ -1,9 +0,0 @@
-#ifndef MoreUnits_H
-#define MoreUnits_H 1
-
-const G4double in = 2.54*cm; // Inch
-const G4double ft = 12*in;   // Foot
-const G4double yd = 3*ft;    // Yard
-
-
-#endif
