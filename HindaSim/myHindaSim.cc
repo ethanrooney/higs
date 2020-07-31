78c94befce0bbee7c1bb2a150e8a0afadc736938
Ethan Rooney <ethan.f.rooney@protonmail.com>
2020-07-30 23:25:11 -0400

    worked on Hinda

--- HindaSim/myHindaSim.cc
+++ HindaSim/myHindaSim.cc
@@ -18,15 +18,17 @@
 //#include "LHEP_PRECO_HP.hh"
 #include "QGSP_BIC_HP.hh"
 
-#ifdef G4VIS_USE
 #include "G4VisExecutive.hh"
-#endif
 #include "G4UIExecutive.hh"
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 int main(int argc,char** argv)
 {
+  G4UIExecutive* ui = 0;
+  if ( argc == 1 ) {
+    ui = new G4UIExecutive(argc, argv);
+  }
   // Run manager
   //
   G4RunManager * runManager = new G4RunManager;
@@ -65,6 +67,13 @@ int main(int argc,char** argv)
   // Get the pointer to the User Interface manager
   G4UImanager * UI = G4UImanager::GetUIpointer();  
 
+  // Initialize visualization
+  //
+  G4VisManager* visManager = new G4VisExecutive;
+  // G4VisExecutive can take a verbosity argument - see /vis/verbose guidance.
+  // G4VisManager* visManager = new G4VisExecutive("Quiet");
+  visManager->Initialize();
+
   if (argc!=1)   // batch mode  
     {
      G4String command = "/control/execute ";
@@ -73,36 +82,17 @@ int main(int argc,char** argv)
     }
     
   else           // interactive mode : define visualization and UI terminal
-    { 
-#ifdef G4VIS_USE
-      G4VisManager* visManager = new G4VisExecutive;
-      visManager->Initialize();
-#endif    
-     
-      G4UIsession * session = 0;
-#ifdef G4UI_USE_TCSH
-      session = new G4UIterminal(new G4UItcsh);      
-#else
-      session = new G4UIterminal();
-#endif
-
-#ifdef G4VIS_USE
-      UI->ApplyCommand("/control/execute macros/vis.mac");     
-#endif
-
-
-      session->SessionStart();
-      delete session;
-     
-#ifdef G4VIS_USE
-      delete visManager;
-#endif     
+    {
+      UI->ApplyCommand("/control/execute init_vis.mac");
+      ui->SessionStart();
+      delete ui;
     }
 
   // Free the store: user actions, physics_list and detector_description are
   //                 owned and deleted by the run manager, so they should not
   //                 be deleted in the main() program !
 
+  delete visManager;
   delete runManager;
 
   return 0;
